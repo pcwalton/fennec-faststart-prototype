189e414f83c963689ec252418abb89f84789a519
Patrick Walton <pcwalton@mimiga.net>
2011-10-07 17:04:53 -0700

    Surface testing

--- src/org/mozilla/fennecfaststart/FennecFaststartActivity.java
+++ src/org/mozilla/fennecfaststart/FennecFaststartActivity.java
@@ -1,15 +1,17 @@
 package org.mozilla.fennecfaststart;
 
+import org.mozilla.fennecfaststart.MainUIController;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class FennecFaststartActivity extends Activity
-{
+public class FennecFaststartActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        MainUIController controller = new MainUIController(this);
+        setContentView(controller.getOuterView());
+        controller.start();
     }
 }
---
+++ src/org/mozilla/fennecfaststart/MainUIController.java
@@ -0,0 +1,87 @@
+/* -*- Mode: Java; c-basic-offset: 4; tab-width: 20; indent-tabs-mode: nil; -*-
+ * ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Mozilla Android code.
+ *
+ * The Initial Developer of the Original Code is Mozilla Foundation.
+ * Portions created by the Initial Developer are Copyright (C) 2009-2010
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Patrick Walton <pcwalton@mozilla.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+package org.mozilla.fennecfaststart;
+
+import org.mozilla.fennecfaststart.AwesomeBarController;
+import org.mozilla.fennecfaststart.GeckoSurfaceView;
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class MainUIController {
+    private Activity activity;
+    private SurfaceTestController mController;
+    private View outerView;
+
+    public MainUIController(Activity inActivity) {
+        activity = inActivity;
+        build();
+    }
+
+    public Activity getActivity() { return activity; }
+
+    /** Constructs the UI. */
+    private void build() {
+        AwesomeBarController awesomeBarController =
+            new AwesomeBarController(this);
+
+        // Content
+        // GeckoSurfaceView contentView = new GeckoSurfaceView(activity);
+        mController = new SurfaceTestController(activity);
+        View contentView = mController.getLayout();
+        LinearLayout.LayoutParams contentViewLayout =
+            new LinearLayout.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT,
+                                          ViewGroup.LayoutParams.FILL_PARENT);
+        contentViewLayout.weight = 1.0f;
+        contentView.setLayoutParams(contentViewLayout);
+
+        LinearLayout outerLayout = new LinearLayout(activity);
+        outerLayout.setOrientation(LinearLayout.VERTICAL);
+        outerLayout.addView(awesomeBarController.getAwesomeBar());
+        outerLayout.addView(contentView);
+
+        outerView = outerLayout;
+    }
+
+    public View getOuterView() { return outerView; }
+
+    public void start() { mController.start(); }
+}
+
